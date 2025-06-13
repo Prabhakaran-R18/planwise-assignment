27792787c374e7e30479339b382e552261d9eb73
Prabhakaran <prabhamalathi5@gmail.com>
2025-06-14 00:09:02 +0530

    analytics checking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-//app.js
 import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
